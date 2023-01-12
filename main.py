f474f3061ea892a26d83851d1c0159300c3c3ea7
Miron <miron@users.noreply.github.com>
2023-01-12 18:44:49 +0000

    p

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ app = FastAPI()
 @app.post("/gps/")
 def handle_gps_data(latitude: float = Body(...), longitude: float = Body(...)):
     # Do something with the location data
+    print({"latitude": latitude, "longitude": longitude})
     return {"latitude": latitude, "longitude": longitude}
